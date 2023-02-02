ff6ccbd1db6fc794b27615637a7460a083271623
andrey zobnin <andreyz3000@ya.ru>
2023-02-02 13:48:35 +0300

    Домашка 6 java

--- Seminar6Task/Program.java
+++ Seminar6Task/Program.java
@@ -1,8 +1,10 @@
+/* Разработать программу, имитирующую поведение коллекции HashSet. 
+В программе содать метод add добавляющий элемент, и метод позволяющий 
+выводить эллементы коллекции в консоль. Формат данных Integer. */
+
 package Seminar6Task;
 import java.util.HashMap;
 import java.util.Map;
-
-
 public class Program {
     private static Object OBJECT;
     private Map<Integer, Object> setClone;
@@ -12,25 +14,13 @@ public class Program {
     }
 
     public static void main(String[] args) {
-//  Разработать программу, имитирующую поведение коллекции HashSet.
-//  В программе содать метод add добавляющий элемент,
-//  и метод позволяющий выводить эллементы коллекции в консоль.
-//  Формат данных Integer.
-  /*   Set<Integer> set = new HashSet<>();
-     set.add(22);
-     set.add(82);
-     set.add(224);
-     set.add(224);
-     set.add(212);
-     set.forEach(integer -> System.out.println(integer));
- */
+
         Program tipoSet = new Program();
         tipoSet.add(12);
         tipoSet.add(66);
         tipoSet.add(135);
         tipoSet.add(200);
-        tipoSet.add(299);
-
+        
         System.out.println(tipoSet);
     }
 
